a579d3eecd86a4268c886b4e75923150fd720d32
William K <wkkerns@gmail.com>
2015-03-10 10:05:29 -0400

    App Live - DB change

--- app/config/live/database.php
+++ app/config/live/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'wellness',
+			'database'  => 'wellnessv2',
 			'username'  => 'root',
 			'password'  => 'otad300',
 			'charset'   => 'utf8',
